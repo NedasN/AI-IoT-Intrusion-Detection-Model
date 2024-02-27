582abdd7558044ee5624e7dc6e048919661fc435
Nedas Necepurenko <nedas@Nedass-MacBook-Pro.local>
2024-02-27 15:47:33 +0000

    Should just need to let run

--- DataNormalisation.py
+++ DataNormalisation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import torch
 from sklearn.model_selection import train_test_split
 
 def processData():
@@ -13,7 +14,7 @@ def processData():
     #data = dn.StringsToCategories(data)
     protocol_map = {'tcp': 1, 'udp': 2, 'icmp': 3}
     service_map = {'-': 0, 'http': 1, 'dns' : 2, 'ftp' : 3, 'smb' : 4, 'ssl' : 5, 'dhcp' : 6, 'gssapi' : 7, 'smb;gssapi' : 8,'dce_rpc' : 9}
-    conn_state_map = {'OTH': 0, 'REJ': 1, 'RSTO': 2, 'RSTOS0': 3, 'RSTR': 4, 'RSTRH': 5, 'S0': 6, 'S1': 7, 'S2': 8, 'S3': 9, 'SF': 10, 'SH' : 11, 'SH' : 12}
+    conn_state_map = {'OTH': 0, 'REJ': 1, 'RSTO': 2, 'RSTOS0': 3, 'RSTR': 4, 'RSTRH': 5, 'S0': 6, 'S1': 7, 'S2': 8, 'S3': 9, 'SF': 10, 'SH' : 11, 'SHR' : 12}
     data['proto'] = data['proto'].map(protocol_map)
     data['service'] = data['service'].map(service_map)
     data['conn_state'] = data['conn_state'].map(conn_state_map)
@@ -30,7 +31,8 @@ def processData():
     data['weird_addl'] = data['weird_addl'].astype(int)
 
     #print(data['weird_addl'].unique())
-
+    #print(data['conn_state'].unique())
+    '''
     # Split data into train/test(70/30) split
     train, test = train_test_split(data, test_size=0.3, random_state=42, shuffle=True)
     print("Split the dataset into train and test")
@@ -39,4 +41,18 @@ def processData():
     train = train.drop(train.columns[-1], axis=1)
     test = test.drop(test.columns[-1], axis=1)
     #print(train.dtypes)
-    return train, test, labels
+    train_tensor = torch.tensor(train.values)
+    target_tensor = torch.tensor(labels.values)
+    '''
+    #Get the tensor of the data as well as the tensor containing target values
+    #duration_tensor = torch.tensor(data['duration'].values)
+    #num_bins = int(duration_tensor.size(0) ** 0.5)
+    #duration_discretized = torch.bucketize(duration_tensor, boundaries=torch.linspace(duration_tensor.min(), duration_tensor.max(), steps=num_bins))
+    target_tensor = torch.tensor(data['label'].values)
+    data.drop(['label'], axis=1, inplace=True)
+    #data.drop(['duration'], axis=1, inplace=True)
+    train_tensor = torch.tensor(data.values, requires_grad=False).float()
+    #train_tensor = torch.cat((train_tensor, duration_discretized.unsqueeze(1)), 1)
+    #print(train_tensor.dtype)
+    #print(target_tensor.dtype)
+    return train_tensor, target_tensor
--- model.py
+++ model.py
@@ -14,21 +14,23 @@ class MyNeuralNetwork(nn.Module):
         super(MyNeuralNetwork, self).__init__()
         self.layer1 = nn.Linear(30, 73, bias=True)  # First hidden layer with 73 neurons
         self.layer2 = nn.Linear(73, 69, bias=True)  # Second hidden layer with 69 neurons
-        self.layer3 = nn.Linear(69, 2, bias=True)   # Output layer with 2 neurons
-
+        self.layer3 = nn.Linear(69, 1, bias=True)  # Third hidden layer with 48 neurons
+        #self.layer3 = nn.Linear(69, 48, bias=True)  # Third hidden layer with 48 neurons
+        #self.layer4 = nn.Linear(48, 34, bias=True)  # Fourth hidden layer with 34 neurons
+        #self.layer5 = nn.Linear(34, 1, bias=True)   # Output layer
+    
     def forward(self, x):
-        x = torch.relu(self.layer1(x))
-        x = torch.relu(self.layer2(x))
-        x = torch.sigmoid(self.layer3(x))
+        x = self.layer1(x)
+        x = torch.relu(x)
+        x = self.layer2(x)
+        x = torch.sigmoid(x)
         return x
 
+
 #process the data and get the train test split
-train, test, targets = dn.processData()
+train_tensor, target_tensor = dn.processData()
 #print(train.dtypes)
 #print(train.head())
-#print(test.head())
-train_tensor = torch.tensor(train.values)
-#test = torch.tensor(test.values)
 #print(train_tensor.dtype)
 
 #Model and optimiser
@@ -44,15 +46,24 @@ optim = ParticleSwarmOptimizer(
     min_param_value=-10000
 )
 
-criterion = torch.nn.MSELoss()
+#print("Predictions",model(train_tensor))
+#print("Target", target_tensor)
+criterion = nn.CrossEntropyLoss()
 
-for _ in range(100):
-    
+#print(model(train_tensor).size())
+
+print("Got to before the loop")
+for epoch in range(500):
+    print("in the loop now")
     def closure():
         # Clear any grads from before the optimization step, since we will be changing the parameters
-        optim.zero_grad()  
-        return criterion(model.forward(train_tensor), targets)
+        optim.zero_grad()
+        out = model(train_tensor)
+        #out = out.view(-1)
+        print("in Closure")
+        return criterion(out, target_tensor)
     
     optim.step(closure)
-    print('Prediciton', model.forward(train_tensor))
-    print('Target    ', targets)
+    print(f"Epoch {epoch}/500")
+    #print('Prediction', model(train_tensor))
+    #print('Target    ', target_tensor)
