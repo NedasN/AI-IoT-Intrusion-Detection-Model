697aef66733387c00d6a3128d4658c20279b10c1
nn2001 <nn2001@hw.ac.uk>
2024-02-27 21:31:44 +0000

    Saving old code, incase can fix the issue

--- oldModel.py
+++ oldModel.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import torch
 import torchswarm
+from torch_pso import ParticleSwarmOptimizer
 import torch.nn as nn
 import torch.optim as optim
 import DataNormalisation as dn
@@ -42,10 +43,17 @@ train_tensor, target_tensor = dn.processData()
 model = MyNeuralNetwork()
 model.to(device)
 
+optim = ParticleSwarmOptimizer(model.parameters(),
+                            inertial_weight=0.7,
+                            cognitive_coefficient=0.8,
+                            social_coefficient=0.9,
+                            num_particles=1000,
+                            max_param_value=10000,
+                            min_param_value=-10000)
 
 #print("Predictions",model(train_tensor))
 #print("Target", target_tensor)
-
+criterion = nn.MSELoss()
 train_tensor = train_tensor.to(device)
 target_tensor = target_tensor.to(device)
 #print(model(train_tensor))
@@ -53,6 +61,12 @@ target_tensor = target_tensor.to(device)
 #train the model
 for epoch in range(250):
     #ask copilot on how to use torchswarm to train a neural network
+    def closure():
+        optim.zero_grad()
+        output = model(train_tensor)
+
+        return criterion(output, target_tensor)
+    optim.step(closure)
 
     print(f"Epoch {epoch+1}/250")
     #print('Prediction', model(train_tensor))
