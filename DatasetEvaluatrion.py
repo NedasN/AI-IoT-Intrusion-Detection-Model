9be660c70021ec8ceec44ce9a6ff5129bb4f122e
nn2001 <nn2001@hw.ac.uk>
2024-04-01 17:07:56 +0100

    adjusted plots

--- DatasetEvaluatrion.py
+++ DatasetEvaluatrion.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('Dataset/TON_IoT/Train_Test_Network.csv')
 
-count = data['type'].value_counts()
+count = data['label'].value_counts()
 
-count.plot(kind='bar')
-
-plt.title('Distribution of the labels')
+count.plot(kind='pie', autopct='%1.1f%%')
+plt.ylabel('')
+plt.title('Packet Balance in the Dataset')
 plt.show()
